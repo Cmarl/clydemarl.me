1eba3a500279740246d1a890f336fa9d7545517a
Clyde G Marl <marl.clyde@gmail.com>
2015-04-07 18:45:14 -0700

    convert html to less, fix conersion format issues

--- client/index.js
+++ client/index.js
@@ -1,2 +1,8 @@
 'use strict';
-console.log('apollo -> online');
+
+$(document).ready(function() {
+  $('footer').on('click','button',function() {
+    var clicked = $(this).data();
+    $('body').removeClass().addClass(clicked.theme);
+  });
+});
